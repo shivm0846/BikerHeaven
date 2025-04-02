5fc99960e7f200fbb4ccd5bff2efa367ceb3ae07
shivm0846 <2192827s@gmail.com>
2025-04-02 13:17:07 +0530

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+let a=10;
+let b=50;
+let c=a+b;
+
+console.log(c);
+console.log("Current Directory:",__dirname);
+console.log("Current File:",__filename);
+setTimeout(() => console.log("Hello word ", 5000))
